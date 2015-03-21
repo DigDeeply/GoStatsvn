7c8721d75d26b3020c01caad4f1c00d6df5e2339
Fukun <kiqq2003@163.com>
2015-03-21 22:33:20 +0800

    add stat by hour, without duplicate removal. todo

--- GoStatsvn/main.go
+++ GoStatsvn/main.go
@@ -8,6 +8,11 @@ import(
 	"util"
 	"strconv"
 	"statStruct"
+	"time"
+)
+
+const (
+	DEFAULT_SMALLEST_TIME_STRING = "1000-03-20T08:38:17.428370Z"
 )
 
 var svnXmlFile *string = flag.String("f", "", "svn log with xml format")
@@ -59,6 +64,7 @@ func main() {
 				appendLines, removeLines, err := util.GetLineDiff(stdout)
 				fmt.Printf("\t%s on r%d +%d -%d,\n", path.Path, newRev, appendLines, removeLines)
 				if err == nil {
+					//综合统计
 					Author, ok := AuthorStats[svnXmlLog.Author]
 					if ok {
 						Author.AppendLines += appendLines
@@ -68,6 +74,9 @@ func main() {
 						Author.RemoveLines = removeLines
 					}
 					AuthorStats[svnXmlLog.Author] = Author
+					//分时统计
+					Author.AppendLines = appendLines
+					Author.RemoveLines = removeLines
 					authorTimeStat := make(statStruct.AuthorTimeStat)
 					authorTimeStat[svnXmlLog.Date] = Author
 					authorTimeStats[svnXmlLog.Author] = append(authorTimeStats[svnXmlLog.Author], authorTimeStat)
@@ -76,9 +85,10 @@ func main() {
 			}
 		}
 	}
-	fmt.Println(authorTimeStats)
 	//输出结果
 	ConsoleOutPutTable(AuthorStats)
+	//输出按小时统计结果
+	ConsoleOutPutHourTable(authorTimeStats)
 
 }
 
@@ -86,6 +96,38 @@ func main() {
 func ConsoleOutPutTable(AuthorStats map[string]statStruct.AuthorStat) {/*{{{*/
 	fmt.Printf(" ==user== \t==lines==\n")
 	for author, val := range AuthorStats {
-		fmt.Printf("%10s\t%d\n", author, val.AppendLines+val.RemoveLines)
+		fmt.Printf("%10s\t%5d\n", author, val.AppendLines+val.RemoveLines)
+	}
+}/*}}}*/
+
+//console按小时输出结果
+func ConsoleOutPutHourTable(authorTimeStats statStruct.AuthorTimeStats) {/*{{{*/
+	defaultSmallestTime, _ := time.Parse("2006-01-02T15:04:05Z", DEFAULT_SMALLEST_TIME_STRING)
+	fmt.Printf(" ==user== \t==hour==\t==lines==\n")
+	//先取到时间的区间值
+	for authorName, Author := range authorTimeStats {
+		var minTime time.Time
+		var maxTime time.Time
+		for _, t := range Author {
+			for sTime,_ := range t {
+				fmtTime, err := time.Parse("2006-01-02T15:04:05Z", sTime)
+				util.CheckErr(err)
+				if minTime.Before(defaultSmallestTime) || minTime.After(fmtTime) {
+					minTime = fmtTime
+				}
+				if maxTime.Before(defaultSmallestTime) || maxTime.Before(fmtTime) {
+					maxTime = fmtTime
+				}
+			}
+		}
+		//Todo 用户按时合并,去重
+		//输出单个用户的数据
+		for _, t := range Author {
+			for sTime,Sval := range t {
+				fmtTime, err := time.Parse("2006-01-02T15:04:05Z", sTime)
+				util.CheckErr(err)
+				fmt.Printf("%10s\t%5d\t%12d\n", authorName, fmtTime.Hour(), Sval.AppendLines+Sval.RemoveLines)
+			}
+		}
 	}
 }/*}}}*/
